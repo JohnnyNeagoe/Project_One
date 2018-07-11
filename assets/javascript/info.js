ddedf066efb10cd69a97ee6af5640d04960b8a69
unknown <msvendsentan@gmail.com>
2018-07-10 21:41:59 -0400

    minimum viable product

--- assets/javascript/info.js
+++ assets/javascript/info.js
@@ -1,8 +1,8 @@
 // Declare Variables
-var userDestination = ["London", "England"];
+var userDestination = ["London", "United Kingdom"];
 var userLocation = ["Toronto", "Canada"];
 
-var restCountriesURL = "https://restcountries.eu/rest/v2/name/" + userDestination[1];
+var restCountriesURL;
 var geonamesURL;
 var wikipediaURL;
 var wikipediaURL2;
@@ -23,11 +23,11 @@ var placesQueryURL;
 var photosQueryURL;
 
 
+var newsAPIKey = "6bf648adc8a74ad8830c059dea6040e1";
 
-
-
-
-
+//https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=6bf648adc8a74ad8830c059dea6040e1
+var newsQueryURL;
+var newsDate;
 
 // Declare Functions
 function googlePlacesQuery() {
@@ -49,7 +49,6 @@ function populatePhotoArray() {
         url: placesQueryURL,
         method: "GET"
     }).then(function(response) {
-        console.log(response);
         for (var i = 0; i < response.result.photos.length; i++) {
             googlePhotosArray.push(response.result.photos[i].photo_reference);
         }
@@ -59,13 +58,13 @@ function populatePhotoArray() {
 
 function populatePhotoCarousel() {
 
-    console.log(googlePhotosArray);
-
     for (var i = 0; i < googlePhotosArray.length; i++) {
-        photosQueryURL = "https://maps.googleapis.com/maps/api/place/photo?maxheight=400&photoreference=" + googlePhotosArray[i] + "&key=" + googleAPIKey;
-        $("#city_photo_carousel").append("<img src=" + photosQueryURL + ">");
-
+        photosQueryURL = "https://maps.googleapis.com/maps/api/place/photo?maxheight=300&photoreference=" + googlePhotosArray[i] + "&key=" + googleAPIKey;
+        $("<div>").addClass("carousel-item").append("<img class=\"d-block carousel_image\" src=" + photosQueryURL + ">").appendTo(".carousel-inner");
+        //$("#city_photo_carousel").append("<img class=\"carousel_image\" src=" + photosQueryURL + ">");
     }
+
+    $(".carousel-item:first").addClass("active");
 
 }
 
@@ -91,6 +90,75 @@ function wikipediaQuery() {
     });
 }
 
+function newsQuery() {
+    newsQueryURL = "https://newsapi.org/v2/everything?q=(" + userDestination[0] + "%20and%20" + userDestination[1] + ")&sortBy=popularity&pageSize=10&apiKey=" + newsAPIKey;
+    console.log(newsQueryURL);
+
+    $.ajax({
+        url: newsQueryURL,
+        method: "GET"
+    }).then(function(response) {
+       
+        for (var i = 0; i < response.articles.length; i++) {
+           
+            var storyContainer = $("<div>");
+            storyContainer.addClass("story_container");
+
+            var storyURL = $("<a>").attr("href", response.articles[i].url);
+            storyURL.append(response.articles[i].title);
+
+            var storyTitle = $("<div>");
+            storyTitle.addClass("story_title").append(storyURL);
+    
+            var storyDescription = $("<div>");
+            storyDescription.addClass("story_description").append("<p>" + response.articles[i].description + "</p>");
+    
+            storyContainer.append(storyTitle).append(storyDescription).appendTo("#stories_list");
+        }
+    });
+}
+
+function countryInfoQuery() {
+    restCountriesURL = "https://restcountries.eu/rest/v2/name/" + userDestination[1];
+
+    $.ajax({
+        url: restCountriesURL,
+        method: "GET"
+    }).then(function(response) {
+
+        $("#country_info_container")
+            .append("<h2>A snapshot of " + response[0].name + "</h2>")
+            .append("<img class=\"flag\" src=" + response[0].flag + ">")
+            .append("<p><b>Primary Language:</b> " + response[0].languages[0].name + "</p>")
+            .append("<p><b>Capital:</b> " + response[0].capital + "</p>")
+            .append("<p><b>Primary Currency:</b> " + response[0].currencies[0].symbol + " " + response[0].currencies[0].name + "</p>")
+
+    });
+
+}
+
+//Unnecessary because free news plan is max 1 month in the past.
+/* function dateBacktrack() {
+    var dateObj = new Date();
+    var month = dateObj.getUTCMonth() + 1; //months from 1-12
+    var day = dateObj.getUTCDate();
+    var year = dateObj.getUTCFullYear();
+
+    var monthDelay = 1;
+
+    var newMonth = month - monthDelay;
+    var newDay = day;
+    var newYear = year;
+
+    if (newMonth < 1) {
+        newMonth = newMonth + 12;
+        newYear = newYear - 1;
+    }
+
+    newsDate = newYear + "-" + newMonth + "-" + newDay;
+    console.log(newsDate);
+} */
+
 
 
 
@@ -103,6 +171,9 @@ $(document).ready(function() {
 
     googlePlacesQuery();
     wikipediaQuery();
+    countryInfoQuery();
+    newsQuery();
+
 
 
 
